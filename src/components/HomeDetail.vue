e1c2ea4f619e76f959dcce1326b804228aaf3a89
guoling1 <646626114@qq.com>
2018-08-25 17:41:42 +0800

    test

--- src/components/HomeDetail.vue
+++ src/components/HomeDetail.vue
@@ -332,10 +332,19 @@
         }else if(key=="menu2"){
           num = '02'
         }
+        var color;
+        if(this.color=='请选择'){
+          color=''
+        }else {
+          color = 'red';
+          // color = this.color;
+        }
         let params = {
           netMode: num,
           // mobile:'',
-          // productId:this.id
+          productId:this.id,
+          productColor: color,
+          areaCode: '2'
         }
         this.$axios.post("/open/busi/netQueryParams", params)
           .then(res => {
@@ -353,27 +362,27 @@
               document.getElementsByTagName('html')[0].appendChild(form)
               form.submit();
             }
-            var data = JSON.parse(res.data);
-
-            data.productId = this.id;
-            var color;
-            if(_this.color=='请选择'){
-              color=''
-            }else {
-              color = _this.color;
-            }
-            data.productColor = color;
-            data.areaCode = '2';
-            function objKeySort(obj) {//排序的函数
-              var newkey = Object.keys(obj).sort();
-              //先用Object内置类的keys方法获取要排序对象的属性名，再利用Array原型上的sort方法对获取的属性名进行排序，newkey是一个数组
-              var newObj = {};//创建一个新的对象，用于存放排好序的键值对
-              for (var i = 0; i < newkey.length; i++) {//遍历newkey数组
-                newObj[newkey[i]] = obj[newkey[i]];//向新创建的对象中按照排好的顺序依次增加键值对
-              }
-              return newObj;//返回排好序的新对象
-            }
-            standardPost('http://60.10.25.233:443/jkzyApp/woJXController/index.shtml', {msg:JSON.stringify(objKeySort(data))})
+            // var data = JSON.parse(res.data);
+            //
+            // data.productId = this.id;
+            // var color;
+            // if(_this.color=='请选择'){
+            //   color=''
+            // }else {
+            //   color = _this.color;
+            // }
+            // data.productColor = color;
+            // data.areaCode = '2';
+            // function objKeySort(obj) {//排序的函数
+            //   var newkey = Object.keys(obj).sort();
+            //   //先用Object内置类的keys方法获取要排序对象的属性名，再利用Array原型上的sort方法对获取的属性名进行排序，newkey是一个数组
+            //   var newObj = {};//创建一个新的对象，用于存放排好序的键值对
+            //   for (var i = 0; i < newkey.length; i++) {//遍历newkey数组
+            //     newObj[newkey[i]] = obj[newkey[i]];//向新创建的对象中按照排好的顺序依次增加键值对
+            //   }
+            //   return newObj;//返回排好序的新对象
+            // }
+            standardPost('http://60.10.25.233:443/jkzyApp/woJXController/index.shtml', {msg:res.data})
           })
       },
       toSelectPhone() {
